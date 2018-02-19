9f2723f374c33c7d9ed71645e9494d76dbbbf3b0
zhaoxiaojian <zhaoxiaojian@hotmail.com>
2018-02-19 11:14:48 +0800

    Initial Commit

---
+++ game_tetris/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  game_tetris
+//
+//  Created by zhaoxiaojian on 2/19/18.
+//  Copyright © 2018 Zhao Xiaojian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ game_tetris/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  game_tetris
+//
+//  Created by zhaoxiaojian on 2/19/18.
+//  Copyright © 2018 Zhao Xiaojian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
